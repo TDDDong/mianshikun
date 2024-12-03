906ea5f6375d74e599bf14d8eccd56ddcdf659bc
12618 <1261801883@qq.com>
2024-12-03 20:58:04 +0800

    2024-12-03 1.引入sentinel,自定义资源并对其进行流控/降级的规则配置

--- src/main/java/com/dd/mianshikun/config/RedissonConfig.java
+++ src/main/java/com/dd/mianshikun/config/RedissonConfig.java
@@ -8,8 +8,6 @@ import org.redisson.config.Config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 全局跨域配置
